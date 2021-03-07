c32dbd06e29bac8013000926351121f9ebe566a6
Andrea Evelyn Mejía Rubio <andiemartin33@gmail.com>
2021-03-07 02:27:36 -0600

    Agregar archivos de proyecto.

---
+++ mayor y menor/Mayormenor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mayor_y_menor
+{
+    class Mayormenor
+    {
+        double numero1;
+        double numero2;
+
+
+        public void selenumero()
+        {
+            Console.WriteLine("Dame el primer numero: ");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Dame el segundo numero: ");
+            numero2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+            if(numero1 > numero2)
+            {
+                Console.WriteLine("El primer numero es el mayor, el cual es {0}", numero1);
+
+            }
+            else
+                Console.WriteLine("El segundo numero es el mayor, el cual es {0}", numero2);
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ mayor y menor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mayor_y_menor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mayormenor uwu = new Mayormenor();
+
+            uwu.selenumero();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
